30f019959a1c80db21373c9ec0286ed6748d1ebb
pranay1260 <143398234+pranay1260@users.noreply.github.com>
2026-03-25 10:23:01 +0530

    second commit

--- src/main/java/com/jsw/project_java/payment/CardPayment.java
+++ src/main/java/com/jsw/project_java/payment/CardPayment.java
@@ -1,12 +1,30 @@
 package com.jsw.project_java.payment;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy  // object will be created only when needed
 public class CardPayment implements Payment {
+
+    public CardPayment() {
+        System.out.println("CardPayment Constructor called");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("CardPayment Initialized");
+    }
 
     @Override
     public String pay() {
         return "Paid using Card";
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("CardPayment Destroyed");
     }
 }
